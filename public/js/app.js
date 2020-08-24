cb050e1da77af44c449c6d78825bff436ceed651
Om Mahawar <ommahawar76@gmail.com>
2020-08-25 00:49:24 +0530

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+// console.log('Client side js loaded!')
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+let messageOne = document.getElementById('message-1');
+let messageTwo = document.getElementById('message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${location.trim()}`).then(
+    (res) => {
+      res.json().then((data) => {
+        if (data.error) {
+          messageOne.innerText = data.error;
+          messageTwo.innerText = '';
+        } else {
+          messageOne.innerText = data.location;
+          messageTwo.innerText = data.forecast;
+        }
+      });
+    }
+  );
+});
